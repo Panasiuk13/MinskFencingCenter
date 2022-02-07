74f076d4291a9802fa685a7ad278e81348299b7d
arturkulik <arturkulik70@gmail.com>
2022-02-07 09:22:22 +0300

    Created modules for different pages

---
+++ refactored-project/src/app/timetable/timetable.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TimetableComponent } from './timetable.component';
+
+
+
+@NgModule({
+  declarations: [
+    TimetableComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TimetableModule { }
